2d66791cf0c241eab00643416cbee5a7c371f7ff
Guilherme Freitas <guilherme.f@onerhino.com>
2024-03-09 10:45:46 -0300

    tokens

---
+++ packeges/tokens/src/colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+  white: "#FFF",
+  black: "#000",
+
+  gray100: "#E1E1E6",
+  gray200: "#A9A9B2",
+  gray400: "#7C7C8A",
+  gray500: "#505059",
+  gray600: "#323238",
+  gray700: "#29292E",
+  gray800: "#202024",
+  gray900: "#121214",
+
+  purple300: "rgb(126 34 206);",
+  purple500: "rgb(107 33 168);",
+  purple700: "rgb(88 28 135);",
+  purple900: "rgb(59 7 100)",
+
+  test: "#fff",
+};
---
+++ packeges/tokens/src/fonts.ts
@@ -0,0 +1,4 @@
+export const fonts = {
+  default: "Work Sans, sans-serif",
+  code: "monospace",
+};
